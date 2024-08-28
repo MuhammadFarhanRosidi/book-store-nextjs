c2ac266be13cdea5dd316e2c85c889472ec1bbaa
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-28 23:26:10 +0700

    feat: create button add wishlist component

---
+++ my-app/src/components/addToWishlist.tsx
@@ -0,0 +1,10 @@
+export default function AddToWishlist() {
+  return (
+    <a
+      href="dashboard-manage-task-add.html"
+      className="p-[12px_20px] bg-gradient-to-b from-[#977FFF] to-[#6F4FFF] rounded-full w-full border border-taskia-background-grey font-semibold text-white text-center"
+    >
+      Add Wishlist
+    </a>
+  );
+}
