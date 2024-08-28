70686f14545d8ea7130bda95f8d191b5c9f0286e
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-28 23:26:59 +0700

    feat: create remove wishlist button component

---
+++ my-app/src/components/removeWishlist.tsx
@@ -0,0 +1,10 @@
+export default function RemoveWishlist() {
+  return (
+    <a
+      href="dashboard-manage-task-add.html"
+      className="p-[12px_20px] bg-gradient-to-b from-[#e85151] to-[#ee0612] rounded-full w-full border border-taskia-background-grey font-semibold text-white text-center"
+    >
+      Remove Wishlist
+    </a>
+  );
+}
