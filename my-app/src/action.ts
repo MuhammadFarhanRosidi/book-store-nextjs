8f55b762e56d3f9a876b7843791899ad03b739a7
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-28 23:19:43 +0700

    feat: create action function createUserAction

---
+++ my-app/src/action.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { BASE_URL } from "./constants";
+
+interface PropsFormDataUser {
+  name: FormDataEntryValue | null;
+  username: FormDataEntryValue | null;
+  email: FormDataEntryValue | null;
+  password: FormDataEntryValue | null;
+}
+
+export const createUserAction = async (rawFormData: PropsFormDataUser) => {
+  const res = await fetch(BASE_URL + "/register", {
+    method: "POST",
+    body: JSON.stringify(rawFormData),
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
+  });
+};
