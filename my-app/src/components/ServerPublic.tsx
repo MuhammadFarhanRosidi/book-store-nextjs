161c6b824bc5e296d08e9cc671d434efef43ebca
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-30 11:16:40 +0700

    feat: create server public

---
+++ my-app/src/components/ServerPublic.tsx
@@ -0,0 +1,15 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface ProtectedProps {
+  children: React.ReactNode;
+}
+
+export default function ServerPublic({ children }: ProtectedProps) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("Authorization");
+  if (token) {
+    redirect("/wishlist");
+  }
+  return <>{children}</>;
+}
