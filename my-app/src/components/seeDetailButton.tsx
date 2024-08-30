2ab12030c3829d59be1c9e1c4c5322ca08c9686b
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-30 11:09:58 +0700

    feat: create seeDetailButton component

--- my-app/src/components/seeDetailButton.tsx
+++ my-app/src/components/seeDetailButton.tsx
@@ -2,12 +2,28 @@
 import { ObjectId } from "mongodb";
 import { useRouter } from "next/navigation";
 
-export default function DetailButton({ _id }: { _id: ObjectId }) {
+export interface Product {
+  _id: ObjectId;
+  name: string;
+  slug: string;
+  description: string;
+  excerpt: string;
+  price: number;
+  tags: string[];
+  thumbnail: string;
+  images: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export default function DetailButton({ product }: { product: Product }) {
   const router = useRouter();
+  const slug = product.slug;
   return (
     <button
-      //   onClick={() => router.push(`/products/${_id}`)}
-      className="p-[12px_20px] text-center rounded-full border border-taskia-black w-full font-semibold"
+      onClick={() => router.push(`/products/${slug}-${product._id}`)}
+      // className="p-[12px_20px] text-center rounded-full border border-taskia-black w-full font-semibold"
+      className="p-[12px_20px] bg-gradient-to-b from-[#977FFF] to-[#6F4FFF] rounded-full w-full border border-taskia-background-grey font-semibold text-white text-center"
     >
       See Detail
     </button>
