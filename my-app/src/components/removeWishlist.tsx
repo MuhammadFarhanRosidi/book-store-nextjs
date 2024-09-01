5337339da078a5e4d0ab316eec71a9807d618037
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-09-01 19:25:46 +0700

    feat: create client component button delete

--- my-app/src/components/removeWishlist.tsx
+++ my-app/src/components/removeWishlist.tsx
@@ -1,10 +1,32 @@
-export default function RemoveWishlist() {
+"use client";
+
+import { redirect, useRouter } from "next/navigation";
+
+// async function handleDelete(id: string) {
+//   const router = useRouter();
+//   const res = await fetch("http://localhost:3000/api/wishlist/" + id, {
+//     method: "DELETE",
+//   });
+//   const data = await res.json();
+//   return router.push("/wishlist");
+// }
+
+export default function RemoveWishlist({ productId }: { productId: string }) {
+  const router = useRouter();
+  async function handleDelete() {
+    const res = await fetch("http://localhost:3000/api/wishlist/" + productId, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    console.log(data);
+    return router.push("/wishlist");
+  }
   return (
-    <a
-      href="dashboard-manage-task-add.html"
+    <button
+      onClick={handleDelete}
       className="p-[12px_20px] bg-gradient-to-b from-[#e85151] to-[#ee0612] rounded-full w-full border border-taskia-background-grey font-semibold text-white text-center"
     >
       Remove Wishlist
-    </a>
+    </button>
   );
 }
