9547d696f4840f76287e3a2583602dfa3c7e478d
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-09-01 19:25:15 +0700

    feat: create client component button add

--- my-app/src/components/addToWishlist.tsx
+++ my-app/src/components/addToWishlist.tsx
@@ -1,10 +1,36 @@
-export default function AddToWishlist() {
+"use client";
+
+import { BASE_URL } from "@/constants";
+import { cookies } from "next/headers";
+import { useRouter } from "next/navigation";
+import { useState, FormEvent, ChangeEvent } from "react";
+
+export default async function AddToWishlist({
+  productId,
+}: {
+  productId: string;
+}) {
+  const router = useRouter();
+  const handleAdd = async () => {
+    const res = await fetch(BASE_URL + `/api/wishlist/${productId}`, {
+      method: "POST",
+      body: JSON.stringify(productId),
+      headers: {
+        "Content-Type": "application/json",
+        // Cookie: cookies().toString(),
+      },
+    });
+    router.push("/wishlist");
+    console.log(res);
+  };
+
   return (
-    <a
-      href="dashboard-manage-task-add.html"
-      className="p-[12px_20px] bg-gradient-to-b from-[#977FFF] to-[#6F4FFF] rounded-full w-full border border-taskia-background-grey font-semibold text-white text-center"
+    <button
+      type="submit"
+      onClick={handleAdd}
+      className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
     >
-      Add Wishlist
-    </a>
+      Add to wishlist
+    </button>
   );
 }
