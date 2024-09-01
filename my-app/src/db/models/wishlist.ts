1f3bfbec4389720ced21f5238b001510c81fb64f
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-09-01 19:26:16 +0700

    feat: create aggregation to show wishlist card

--- my-app/src/db/models/wishlist.ts
+++ my-app/src/db/models/wishlist.ts
@@ -22,22 +22,53 @@ export const getDb = async () => {
 
 export const getAllWishtlists = async () => {
   const db = await getDb();
-  const wishtlists = (await db
-    .collection(COLLECTION_WISHLIST)
-    .find()
-    .toArray()) as WishlistModel[];
-  return wishtlists;
+  // const wishtlists = (await db
+  //   .collection(COLLECTION_WISHLIST)
+  //   .find()
+  //   .toArray()) as WishlistModel[];
+  const agg = [
+    {
+      $lookup: {
+        from: "products",
+        localField: "productId",
+        foreignField: "_id",
+        as: "product",
+      },
+    },
+    {
+      $unwind: {
+        path: "$product",
+        includeArrayIndex: "string",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+  ];
+  const coll = db.collection("wishlists");
+  const cursor = coll.aggregate(agg);
+  const result = await cursor.toArray();
+  return result;
 };
 
-export const createWishlist = async (wishtlist: WishlistModelInput) => {
+export const createWishlist = async (wishlist: {
+  userId: string;
+  productId: string;
+}) => {
   const db = await getDb();
+  const modifiedWishtlist: WishlistModelInput = {
+    userId: new ObjectId(wishlist.userId),
+    productId: new ObjectId(wishlist.productId),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
   const newWishlist = await db
     .collection(COLLECTION_WISHLIST)
-    .insertOne(wishtlist);
+    .insertOne(modifiedWishtlist);
   return newWishlist;
 };
 
-export const deleteWishlistById = async (_id: ObjectId) => {
+export const deleteWishlistById = async (_id: string) => {
   const db = await getDb();
-  await db.collection(COLLECTION_WISHLIST).deleteOne({ _id });
+  await db
+    .collection(COLLECTION_WISHLIST)
+    .deleteOne({ _id: new ObjectId(_id) });
 };
